3900ec39a60e06461c9ff1fd6dd31d9cc6d00adb
vicky <lch82327@gmail.com>
2015-11-15 00:17:09 +0800

    recv chat message

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ login(config.FACEBOOK, function callback(err, api) {
     switch (event.type) {
       case 'message':
         if (event.body === '#chat') {
-          command.doChat(event, function(err, success) {
-          });
+          command.doChat(event, api);
         } else if (event.body === '#invite') {
           command.doInvite(event, api);
         } else if (event.body === '#leave') {
