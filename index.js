a2a6fc605e6751365081cd5db638fc39cf746288
vicky <lch82327@gmail.com>
2015-11-15 18:39:39 +0800

    fb bot server finalize

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var login = require('facebook-chat-api');
 var command = require('./lib/command.js');
+var request = require('request');
 var config = require('./lib/config.js');
+
+function pangu(callback) {
+  request.del(
+    {
+      url: `${config.DATABASE}/rest/api/v1/account/pangu`,
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        callback(null, true);
+      } else {
+        callback(new Error(error));
+      }
+    });
+}
+
+pangu((err, res) => {
+  if (!err && res) {
+    console.log('Pangu GO!');
+  }
+});
 
 login(config.FACEBOOK, function callback(err, api) {
   if (err) return console.error(err);
@@ -17,6 +38,9 @@ login(config.FACEBOOK, function callback(err, api) {
         } else if (event.body === '#leave') {
           command.doLeave(event, api);
         } else if (event.body === '#next') {
+          // command.doLeave(event, api);
+          // api.sendMessage('Not implement feature! :( Try typing #chat to find someone again!', event.threadID);
+          // console.log('Not implement feature!');
           command.doNext(event, api);
         } else if (event.body === '#register') {
           command.doRegister(event, api);
